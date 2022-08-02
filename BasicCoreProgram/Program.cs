9619d24db31f529b5287db70063ea959a897e83b
Anisha0906 <anishadas880@gmail.com>
2022-08-02 19:50:10 +0530

    [Anisha] Add: FlipCoin

---
+++ BasicCoreProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Program");
+            Console.WriteLine("Enter your choice between 1 to 10: ");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("1 For: Flip a Coin Head and Tail. ");
+            int selection = Convert.ToInt32(Console.ReadLine());
+            // user selection
+
+            switch (selection)
+            {
+                case 1:
+                    //FLIP COIN
+                    FlipCoinSimulation FlipCoin = new FlipCoinSimulation();
+                    FlipCoin.CheckHeadTailPercentage();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection.");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
